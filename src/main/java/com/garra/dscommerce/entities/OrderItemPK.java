a3a815b40cba8481b4e3b874b4c92e1fdebeb921
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2023-07-27 14:54:15 -0300

    Equals hashCode

--- src/main/java/com/garra/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/garra/dscommerce/entities/OrderItemPK.java
@@ -1,16 +1,18 @@
 package com.garra.dscommerce.entities;
 
+import java.util.Objects;
+
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Embeddable
 public class OrderItemPK {
-	
+
 	@ManyToOne
 	@JoinColumn(name = "order_id")
 	private Order order;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "product_id")
 	private Product product;
@@ -33,7 +35,22 @@ public class OrderItemPK {
 	public void setProduct(Product product) {
 		this.product = product;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
 
 }
