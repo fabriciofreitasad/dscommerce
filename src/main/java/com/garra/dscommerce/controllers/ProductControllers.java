bd5d9bed357e301dd1590b0d2e840a68212ad2e5
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2023-07-29 16:57:10 -0300

    Get product by Id - Get product paged

---
+++ src/main/java/com/garra/dscommerce/controllers/ProductControllers.java
@@ -0,0 +1,33 @@
+package com.garra.dscommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.garra.dscommerce.dto.ProductDTO;
+import com.garra.dscommerce.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductControllers {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		ProductDTO dto = service.findById(id);
+		return dto;
+	}
+	
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
+		
+	}
+
+}
---
+++ src/main/java/com/garra/dscommerce/services/ProductService.java
@@ -0,0 +1,36 @@
+package com.garra.dscommerce.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.garra.dscommerce.dto.ProductDTO;
+import com.garra.dscommerce.entities.Product;
+import com.garra.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Optional<Product> result = repository.findById(id);
+		Product product = result.get();
+		ProductDTO dto = new ProductDTO(product);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = repository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
+	}
+
+}
