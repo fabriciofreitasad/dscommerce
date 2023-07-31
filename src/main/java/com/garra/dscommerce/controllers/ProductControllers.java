65ad6435d09793c799358c2caa792a797f1909a9
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2023-07-31 11:43:45 -0300

    Delete product

--- src/main/java/com/garra/dscommerce/controllers/ProductControllers.java
+++ src/main/java/com/garra/dscommerce/controllers/ProductControllers.java
@@ -1,8 +1,12 @@
 package com.garra.dscommerce.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.garra.dscommerce.dto.ProductDTO;
 import com.garra.dscommerce.services.ProductService;
@@ -22,27 +27,35 @@ public class ProductControllers {
 	private ProductService service;
 	
 	@GetMapping(value = "/{id}")
-	public ProductDTO findById(@PathVariable Long id) {
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
-		return dto;
+		return ResponseEntity.ok(dto);
 	}
 	
 	@GetMapping
-	public Page<ProductDTO> findAll(Pageable pageable) {
-		return service.findAll(pageable);
-		
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+		Page<ProductDTO> dto = service.findAll(pageable);
+		return ResponseEntity.ok(dto);
 	}
 	
 	@PostMapping
-	public ProductDTO isert(@RequestBody ProductDTO dto) {
-		return service.insert(dto);
-		
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ProductDTO update(@PathVariable Long id,@RequestBody ProductDTO dto) {
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id,@RequestBody ProductDTO dto) {
 		dto = service.update(id, dto);
-		return dto;
+		return ResponseEntity.ok(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
