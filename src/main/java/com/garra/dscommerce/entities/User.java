a246acf5f2f9a40ece2e30fce0baac147ab6fa9a
fabricio freitas <fabricio.freitas.ad@gmail.com>
2023-08-20 13:53:16 -0300

    Domain model User Role, seed

--- src/main/java/com/garra/dscommerce/entities/User.java
+++ src/main/java/com/garra/dscommerce/entities/User.java
@@ -2,14 +2,19 @@ package com.garra.dscommerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -21,7 +26,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@Column(unique = true)
 	private String email;
 	private String phone;
@@ -29,8 +34,12 @@ public class User {
 	private String password;
 
 	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new  ArrayList<>();
-	
+	private List<Order> orders = new ArrayList<>();
+
+	@ManyToMany
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
 	public User() {
 	}
 
@@ -96,6 +105,23 @@ public class User {
 		return orders;
 	}
 
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+
+	public boolean hasRole(String rolename) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(rolename)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -112,7 +138,5 @@ public class User {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
 }
