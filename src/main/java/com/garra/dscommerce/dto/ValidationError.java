8931d169d6280483221258a6d34c587fb3f88ee6
fabricio freitas <fabricio.freitas.ad@gmail.com>
2023-08-24 15:43:19 -0300

    FIX validation

--- src/main/java/com/garra/dscommerce/dto/ValidationError.java
+++ src/main/java/com/garra/dscommerce/dto/ValidationError.java
@@ -18,6 +18,7 @@ public class ValidationError extends CustomError {
 	}
 
 	public void addError(String fieldName, String message) {
+		errors.removeIf(x -> x.getFielName().equals(fieldName));
 		errors.add(new FieldMessage(fieldName, message));
 	}
 
